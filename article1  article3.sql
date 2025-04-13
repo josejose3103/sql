ed0827f5ca712bcd7fb997ff2fc88dd7af26d757
josejose3103 <josejose3103@gmail.com>
2025-04-14 07:51:20 +0900

    art1とあｒｔ３のＳＱｌＳＱｌ

--- article1  article3.sql
+++ article1  article3.sql
@@ -45,3 +45,33 @@ WHERE
     t2.noukamei = '坂上孝行'
     AND (t1.id IS NULL)
     AND t2.day BETWEEN '2023-03-26' AND '2023-04-10';
+    SELECT
+    '坂上孝行' AS noukamei,
+    SUM(IFNULL(t1.kingaku, 0)) + SUM(IFNULL(t2.kingaku, 0)) AS total_kingaku
+FROM
+    article1 AS t1
+LEFT JOIN
+    article3 AS t2
+ON
+    t1.noukamei = t2.noukamei
+    AND t1.day = t2.day
+WHERE
+    t1.noukamei = '坂上孝行'
+    AND t1.day BETWEEN '2023-03-26' AND '2023-04-10'
+
+UNION ALL
+
+SELECT
+    '坂上孝行' AS noukamei,
+    SUM(IFNULL(t2.kingaku, 0)) AS total_kingaku
+FROM
+    article3 AS t2
+LEFT JOIN
+    article1 AS t1
+ON
+    t1.noukamei = t2.noukamei
+    AND t1.day = t2.day
+WHERE
+    t2.noukamei = '坂上孝行'
+    AND t1.id IS NULL
+    AND t2.day BETWEEN '2023-03-26' AND '2023-04-10';
